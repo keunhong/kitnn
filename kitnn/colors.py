Fix masking in histogrm matching.

kitnn/colors.py
@@ -160,8 +160,9 @@ def denormalize_lab(lab):
     return lab.view(*lab.size())
 
 
-def hist_match_batch(exemplar_batch, target_batch, exemplar_mask=None,
-                     target_mask=None, lab=True):
+def hist_match_batch(exemplar_batch, target_batch,
+                     exemplar_masks=None,
+                     target_masks=None, lab=True):
     if lab:
         exemplar_batch = rgb_to_lab(exemplar_batch, normalized=True)
         target_batch = rgb_to_lab(target_batch, normalized=True)
@@ -170,9 +171,13 @@ def hist_match_batch(exemplar_batch, target_batch, exemplar_mask=None,
     transferred_patches = []
     for i in range(exemplar_batch.size(0)):
         transferred = np.zeros(target_patches[i].shape)
+        target_mask = None if target_masks is None else target_masks[i]
+        exemplar_mask = None if exemplar_masks is None else exemplar_masks[i]
         for chan in range(3):
-            transferred[:, :, chan] = hist_match(target_patches[i][:, :, chan], exemplar_patches[i][:, :, chan],
-                                                 target_mask, exemplar_mask)
+            transferred[:, :, chan] = hist_match(
+                target_patches[i][:, :, chan],
+                exemplar_patches[i][:, :, chan],
+                target_mask, exemplar_mask)
         transferred_patches.append(transferred)
     transferred_batch = Variable(make_batch(transferred_patches).cuda())
     transferred = transferred_batch.clamp(0, 1)

kitnn/utils.py
@@ -83,14 +83,17 @@ def softmax2d(x):
     return e_x / e_x.sum(axis=-1)[:, :, None]
 
 
-def batch_to_images(batch):
+def batch_to_images(batch, dtype=None):
     if len(batch.size()) == 4:
-        return batch.cpu().data.numpy().reshape(
-            batch.size(0), 3, batch.size(-2), batch.size(-1))\
+        array = batch.cpu().data.numpy().reshape(
+            batch.size(0), batch.size(1), batch.size(-2), batch.size(-1))\
             .transpose((0, 2, 3, 1))
     else:
-        return batch.cpu().data.numpy().reshape(
+        array = batch.cpu().data.numpy().reshape(
             batch.size(0), batch.size(-2), batch.size(-1)).transpose((0, 1, 2))
+    if dtype is not None:
+        return array.astype(dtype=dtype)
+    return array
 
 
 def gradient_image(batch):